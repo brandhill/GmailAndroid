466ca0be56bb0306da441a1a17cd9a1396aa499a
John Vi <kienvtqhi@gmail.com>
2012-11-22 20:17:25 +0700

    add menudrawer.jar library

--- Gmail/src/uet/dtui/gmail/activity/BaseActivityWithMenu.java
+++ Gmail/src/uet/dtui/gmail/activity/BaseActivityWithMenu.java
@@ -1,7 +1,9 @@
 package uet.dtui.gmail.activity;
 
+import net.simonvt.widget.MenuDrawerManager;
 import android.app.Activity;
 
 public class BaseActivityWithMenu extends Activity{
+	public MenuDrawerManager menuDrawer;
 
 }
